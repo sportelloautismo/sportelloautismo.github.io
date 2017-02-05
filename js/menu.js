5cfe681d5cfe17c2022be278eb27ac4917da419a
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-02-05 19:04:41 +0100

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+
+link1 = " <li><a href=\"index.html\">Home</a></li>"
+link2 = ""
+link3 = " <li><a href=\"risorse.html\">Risorse</a></li>"
+link4 = " <li><a href=\"video.html\">Video</a></li>"
+link5 = " <li><a href=\"contact.html\">Contatti</a></li>"
+link6 = " <li><a href=\"about.html\">Chi siamo</a></li>"
+
+document.write(link1+link2+link3+link4+link5+link6)
