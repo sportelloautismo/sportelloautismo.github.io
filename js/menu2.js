92e402cfb7c2339cc338de8b02363b9efc40c768
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-02-06 18:20:50 +0100

    Add files via upload

---
+++ js/menu2.js
@@ -0,0 +1,8 @@
+// menu degli articoli post.html post2.html ...
+    
+document.write("<img src='img/logo.png' width=120>")
+
+link1 = " <a href=\"post.html\">Sportello Autismo ad Ancel Keys</a>"
+link2 = "<a href=\"post2.html\">Poesia sull'autismo</a>"
+
+document.write(link1+link2)
