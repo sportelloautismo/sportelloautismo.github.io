60175c410a548c363bdb2c28823e0c430cd7f290
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-05-25 18:03:07 +0200

    Update gruppoc.js

--- js/gruppoc.js
+++ js/gruppoc.js
@@ -1,1 +1,1 @@
-gruppoc.innerHTML = "Notizie gruppo C";
+gruppo.innerHTML = "<p><iframe width="100%" height="400" src="https://www.youtube.com/embed/hJdG664iams" frameborder="0" allowfullscreen=""></iframe></p>";
