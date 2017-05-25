1b3174a13cb4873b13ece7beafa756d1e4d35b73
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-05-25 06:42:30 +0200

    Update gruppoc.js

--- js/gruppoc.js
+++ js/gruppoc.js
@@ -1,1 +1,13 @@
-gruppoc.innerHTML = "Collegatevi ogni giorno per verificare eventuali cambiamenti nel calendario degli incontri."
+// Notizie nella bacheca dei tre gruppi
+
+if (gruppoa){
+  gruppoa.innerHTML = "Notizie gruppo A";
+}
+
+if (gruppob){
+  gruppob.innerHTML = "Notizie gruppo B";
+}
+
+if (gruppoc){
+  gruppoc.innerHTML = "Collegatevi ogni giorno per verificare eventuali cambiamenti nel calendario degli incontri.";
+}
