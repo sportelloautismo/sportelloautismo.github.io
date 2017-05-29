2f283df2b121d3ea7e9cc55b8329339f1da23ed0
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-05-29 05:48:25 +0200

    Update gruppoc.js

--- js/gruppoc.js
+++ js/gruppoc.js
@@ -1,4 +1,4 @@
-gruppo.iinerHTML = "<h3>29/05/2017</h3>Nuovi materiali aggiunti alla sezione della dott.ssa Quaranta su DSA e PDP. Chi visualizza questi materiali otterrà il nuovo Badge DSA-PDP."
+gruppo.innerHTML = "<h3>29/05/2017</h3>Nuovi materiali aggiunti alla sezione della dott.ssa Quaranta su DSA e PDP. Chi visualizza questi materiali otterrà il nuovo Badge DSA-PDP."
 gruppo.innerHTML +="<h3>28/05/2017 - Nuovo Badge autismo</h3>Aggiunto il badge autismo per chi prende visione dei materiali sull'autismo allegati nella sezione dell'incontro del 31 maggio."
 gruppo.innerHTML +="<h3>26/5/2017 - Nuovo Badge</h1>Aggiunto un nuovo Badge: il Badge Navigatore. Si ottiene navigando nella piattaforma on line del corso per referenti di sostegno. Il tempo trascorso on line lo potete vedere nel registro presenze online."
 gruppo.innerHTML += "<br><img src='http://www.alberghierocastelnuovocilento.gov.it/moodlekeys/pluginfile.php/41/badges/badgeimage/26/f1?refresh=9437'><br>"
