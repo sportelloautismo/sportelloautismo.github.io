6445adc7da28f76c848f98aacdcba67110ebb62d
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-05-31 20:36:45 +0200

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,3 +1,3 @@
-splasha.innerHTML = "Prossimo incontro: 31 maggio - prof. Frolli<br><span class='badge'>New</span>Materiali della dott.ssa Galzerano e Badge Operatori della scuola";
-splashb.innerHTML = "Prossimo incontro: 31 maggio - prof. Frolli<br><span class='badge'>New</span>Materiali della dott.ssa Galzerano e Badge Operatori della scuola";
-splashc.innerHTML = "Prossimo incontro: 31 maggio - prof. Frolli<br><span class='badge'>New</span>Materiali della dott.ssa Galzerano e Badge Operatori della scuola";
+splasha.innerHTML = "<span class='badge'>New</span>Badge internauta";
+splashb.innerHTML = "<span class='badge'>New</span>Materiali della dott.ssa Galzerano e Badge Operatori della scuola";
+splashc.innerHTML = "<span class='badge'>New</span>Materiali della dott.ssa Galzerano e Badge Operatori della scuola";
