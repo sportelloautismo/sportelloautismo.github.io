d743bf16e8829ab5f631cbc32f1967d56cd8bced
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-06-03 08:40:10 +0200

    Update frolli.js

--- corse2/frolli.js
+++ corse2/frolli.js
@@ -1,3 +1,3 @@
 frolli.innerHTML = "Presto in arrivo nuovo materiale video riguardante Frolli"
 bacheca.innerHTML = "Presto in arrivo nuovo materiale video riguardante Frolli"
-gruppo.innerHTML = "<h3>31 giugno 2017</h3>Presto in arrivo nuovi materiali video riguardanti l'incontro con il prof. Frolli" + gruppo.innerHTML
+gruppo2.innerHTML = "<h3>31 giugno 2017</h3>Presto in arrivo nuovi materiali video riguardanti l'incontro con il prof. Frolli"
