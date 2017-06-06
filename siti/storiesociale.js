01acb82d208c5a0b94cb674c9670453c287c5cbd
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-06-06 18:44:42 +0200

    deleted a "

--- siti/storiesociale.js
+++ siti/storiesociale.js
@@ -1,2 +1,2 @@
-storiesociali.innerHTML = "<a href="'http://www.convegni.erickson.it/autismi2016/lutilizzo-storie-sociali-nellautismo/'>Storie sociali dal blog della Erickson</a>";
+storiesociali.innerHTML = "<a href='http://www.convegni.erickson.it/autismi2016/lutilizzo-storie-sociali-nellautismo/'>Storie sociali dal blog della Erickson</a>";
 // copyright Giovanni Gatto
