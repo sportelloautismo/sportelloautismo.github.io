ca93d14b3be6a24d4354b3bf7b8024e026e6ff90
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-06-08 20:52:54 +0200

    Create frolli.js

--- corse2/frolli.js
+++ corse2/frolli.js
@@ -1,2 +1,2 @@
 frolli.innerHTML = "Presto in arrivo nuovo materiale video riguardante Frolli"
-bacheca.innerHTML = "<img src='https://sportelloautismo.github.io/img/summer.png'><br>Presto in arrivo nuovo materiale video riguardante Frolli"
+bacheca.innerHTML = "<img class='img-responsive' src='https://sportelloautismo.github.io/img/summer.png'><br>Presto in arrivo nuovo materiale video riguardante Frolli"
