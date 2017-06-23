bea914dd732acec7105ad668a189e2d0d3fbc0ad
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-06-23 16:29:47 +0200

    Update frolli.js

--- corse2/frolli.js
+++ corse2/frolli.js
@@ -1,2 +1,2 @@
 frolli.innerHTML = "Presto in arrivo nuovo materiale video riguardante Frolli"
-bacheca.innerHTML = "<iframe src='https://www.youtube.com/watch?v=Rt_d5rQ13V0' width=100% height=300><br>Un video sul bullismo che ci è stato segnalato dalla prof. Anna Finamore che ringraziamo."
+bacheca.innerHTML = "<iframe src='https://www.youtube.com/watch?v=Rt_d5rQ13V0' width=100% height=300></iframe><br>Un video sul bullismo che ci è stato segnalato dalla prof. Anna Finamore che ringraziamo."
