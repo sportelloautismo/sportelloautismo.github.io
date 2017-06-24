0c53619b90e4a80925bc74b87f4a4020d3c9c5f9
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-06-24 17:28:41 +0200

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,3 +1,3 @@
-splasha.innerHTML = "<span class='badge'>Pausa estiva</span>A settembre";
-splashb.innerHTML = "<span class='badge'>Pausa estiva</span>A settembre. Presto arriverà il badge 3h";
-splashc.innerHTML = "<span class='badge'>Pausa estiva</span>A settembre. Presto arriverà il Badge 3h";
+splasha.innerHTML = "<span class='badge'>Pausa estiva</span> A settembre";
+splashb.innerHTML = "<span class='badge'>Pausa estiva</span> A settembre. È arrivato il badge 3h.";
+splashc.innerHTML = "<span class='badge'>Pausa estiva</span> A settembre. È arrivato il Badge 3h.";
