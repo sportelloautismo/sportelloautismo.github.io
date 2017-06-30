1d71c8a883edc4c881685b1171a823b59e6e4a97
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-06-30 09:41:50 +0200

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,3 +1,3 @@
-splasha.innerHTML = "<span class='badge'>Pausa estiva</span> Aggiunto un video sulla dislessia nella sezione DSA e PDP";
-splashb.innerHTML = "<span class='badge'>Pausa estiva</span> Aggiunto un video sulla dislessia nella sezione DSA e PDP";
-splashc.innerHTML = "<span class='badge'>Pausa estiva</span> Aggiunto un video sulla dislessia nella sezione DSA e PDP";
+splasha.innerHTML = "<span class='badge'>Pausa estiva</span> Aggiunto un video sulla dislessia nella sezione DSA e PDP e materiali della prof.ssa Chiocca. Presto verrà assegnato un badge per chi prenderà visione del materiale.";
+splashb.innerHTML = "<span class='badge'>Pausa estiva</span> Aggiunto un video sulla dislessia nella sezione DSA e PDP e materiali della prof.ssa Chiocca. Presto verrà assegnato un badge per chi prenderà visione del materiale.";
+splashc.innerHTML = "<span class='badge'>Pausa estiva</span> Aggiunto un video sulla dislessia nella sezione DSA e PDP e materiali della prof.ssa Chiocca. Presto verrà assegnato un badge per prenderà visione del materiale.";
