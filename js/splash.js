ae9db00db628c8842682cc233897b4156881e176
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-07-15 17:56:26 +0200

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,5 +1,5 @@
 var content = "<span class='badge'>New</span>";
-var content += "Un nuovo video sull'autismo (realizzato con la collaborazione del prof. Tony Attwood, esperto dell'autismo)";
+content += "Un nuovo video sull'autismo (realizzato con la collaborazione del prof. Tony Attwood, esperto dell'autismo)";
 var thumbup = "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
 
 splasha.innerHTML = content + thumbup;
