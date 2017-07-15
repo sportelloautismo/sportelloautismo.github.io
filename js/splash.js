1fa1ba61a9a420109f4876845f2d0ebdf534e2f5
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-07-15 19:01:37 +0200

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,5 +1,5 @@
-splasha.innerHTML = "<span class='badge'>New</span>Un nuovo video sull'autismo (realizzato con la collaborazione del prof. Tony Attwood, esperto dell'autismo)<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splasha.innerHTML = "<span class='badge'>Nuovo Badge</span>Nuovo bellissimo Badge sull'autismo (il secondo). Lo potrete ottenere prendendo visione dei nuovi materiali della sezione del prof. Frolli.<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
 
-splashb.innerHTML = "<span class='badge'>New</span>Un nuovo video sull'autismo (realizzato con la collaborazione del prof. Tony Attwood, esperto dell'autismo)<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splashb.innerHTML = "<span class='badge'>Nuovo Badge</span>Nuovo bellissimo Badge sull'autismo (il secondo). Lo potrete ottenere prendendo visione dei nuovi materiali della sezione del prof. Frolli.<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
 
-splashc.innerHTML = "<span class='badge'>New</span>Un nuovo video sull'autismo (realizzato con la collaborazione del prof. Tony Attwood, esperto dell'autismo)<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splashc.innerHTML = "<span class='badge'>Nuovo Badge</span>Nuovo bellissimo Badge sull'autismo (il secondo). Lo potrete ottenere prendendo visione dei nuovi materiali della sezione del prof. Frolli.<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
