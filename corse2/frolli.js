19b96f0d138d04f83742b3c2e2ea2afd6a29f50b
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-07-27 21:24:50 +0200

    Update frolli.js

--- corse2/frolli.js
+++ corse2/frolli.js
@@ -1,2 +1,17 @@
 frolli.innerHTML = "Presto in arrivo nuovo materiale video riguardante Frolli"
+
+// BACHECA DI OGNI CORSO
 bacheca.innerHTML = "<iframe width=\"100%\" height=\"400\" src=\"https://www.youtube.com/embed/kQjtK32mGJQ\" frameborder=\"0\" allowfullscreen></iframe><br>Grazie alla prof. Anna Finamore per averci suggerito questo video."
+
+// SEZIONE NEWS DI OGNI GRUPPO
+
+// sezione per la pagina iniziale con tutte e tre i corsi
+const newstuff = "<span class='badge'>Nuovo Badge</span>";
+const testo = "Un bel video sull'apprendimento (in bacheca) e un collegamento a un docu-film sulla storia di un ragazzo e una ragazza con disabilità (nella sez. sull'autismo del prof. Frolli)";
+const thumb = "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+
+splasha.innerHTML = newstuff + testo + thumb;
+
+splashb.innerHTML = newstuff + testo + thumb;
+
+splashc.innerHTML = newstuff + testo + thumb;
