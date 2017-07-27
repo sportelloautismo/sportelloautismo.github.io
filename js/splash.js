ebb970bdf97476aa8091103791272dd2711492c7
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-07-27 21:19:09 +0200

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,13 +1,9 @@
+const newstuff = "<span class='badge'>Nuovo Badge</span>";
 const testo = "Un bel video sull'apprendimento (in bacheca) e un collegamento a un docu-film sulla storia di un ragazzo e una ragazza con disabilità (nella sez. sull'autismo del prof. Frolli)";
+const thumb = "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
 
-splasha.innerHTML = "<span class='badge'>Nuovo Badge</span>";
-splasha.innerHTML += testo;
-splasha.innerHTML += "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splasha.innerHTML = newstuff + testo + thumb;
 
-splashb.innerHTML = "<span class='badge'>Nuovo Badge</span>";
-splashb.innerHTML += testo;
-splashb.innerHTML += "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splashb.innerHTML = newstuff + testo + thumb;
 
-splashc.innerHTML = "<span class='badge'>Nuovo Badge</span>";
-splashc.innerHTML += testo;
-splashc.innerHTML += "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splashc.innerHTML = newstuff + testo + thumb;
