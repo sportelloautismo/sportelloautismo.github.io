bfd4c871a437acb83b8ec8b6ac4a30e8d70d09b9
sportelloautismo <gattogiovanni@alberghierocastelnuovocilento.gov.it>
2017-07-27 21:00:38 +0200

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,5 +1,13 @@
-splasha.innerHTML = "<span class='badge'>Nuovo Badge</span>Nuovo bellissimo Badge sull'autismo (il secondo). Lo potrete ottenere prendendo visione dei nuovi materiali della sezione del prof. Frolli.<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+const testo = "Un bel video sull'apprendimento (in bacheca) e un collegamento a un docu-film sulla storia di un ragazzo e una ragazza con disabilità (nella sez. sull'autismo del prof. Frolli)";
 
-splashb.innerHTML = "<span class='badge'>Nuovo Badge</span>Nuovo bellissimo Badge sull'autismo (il secondo). Lo potrete ottenere prendendo visione dei nuovi materiali della sezione del prof. Frolli.<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splasha.innerHTML = "<span class='badge'>Nuovo Badge</span>";
+splasha.innerHTML += testo;
+splasha.innerHTML += "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
 
-splashc.innerHTML = "<span class='badge'>Nuovo Badge</span>Nuovo bellissimo Badge sull'autismo (il secondo). Lo potrete ottenere prendendo visione dei nuovi materiali della sezione del prof. Frolli.<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+splashb.innerHTML = "<span class='badge'>Nuovo Badge</span>";
+splashb.innerHTML += testo;
+splashb.innerHTML += "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
+
+splashc.innerHTML = "<span class='badge'>Nuovo Badge</span>";
+splashc.innerHTML += testo;
+splashc.innerHTML += "<span class=\"glyphicon glyphicon-thumbs-up\"></span>";
